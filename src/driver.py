98e13873691a51d1d7f83b8c8e2b06536c7591a9
Leandro <Leandro.guia.dev@gmail.com>
2024-08-16 19:21:00 -0300

    Eliminé más imports olvidados que no se utilizan en drivers.py

--- src/driver.py
+++ src/driver.py
@@ -1,8 +1,6 @@
 from prints import *
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 from selenium.webdriver.firefox.service import Service as fserv
 from selenium.webdriver.chrome.service import Service as chserv
